fdbca51068407fdcf3c3f1cf2b7a14901eb335f2
SHOZAB ANWAR SIDDIQUE <s.nadeem014@yahoo.com>
2023-07-02 14:47:06 +0100

    Close nav bar on section selection on small screens.

--- portfolio/src/components/components/header/Header.jsx
+++ portfolio/src/components/components/header/Header.jsx
@@ -20,37 +20,37 @@ const Header = () => {
           <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
             <ul className="nav__list grid">
               <li className="nav__item">
-                <a href="#home" onClick={() => setActiveNavbar('#home')} className={activeNavbar === '#home' ? "nav__link active-link" : "nav__link"}>
+                <a href="#home" onClick={() => {setActiveNavbar('#home'); showMenu(!Toggle);}} className={activeNavbar === '#home' ? "nav__link active-link" : "nav__link"}>
                   <i className="uil uil-estate nav__icon"></i> Home
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#about" onClick={() => setActiveNavbar('#about')} className={activeNavbar === '#about' ? "nav__link active-link" : "nav__link"}>
+                <a href="#about" onClick={() => {setActiveNavbar('#about'); showMenu(!Toggle);}} className={activeNavbar === '#about' ? "nav__link active-link" : "nav__link"}>
                   <i className="uil uil-user nav__icon"></i>About
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#skills" onClick={() => setActiveNavbar('#skills')} className={activeNavbar === '#skills' ? "nav__link active-link" : "nav__link"}>
+                <a href="#skills" onClick={() => {setActiveNavbar('#skills'); showMenu(!Toggle);}} className={activeNavbar === '#skills' ? "nav__link active-link" : "nav__link"}>
                   <i className="uil uil-file-alt nav__icon"></i>Skills
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#qualification" onClick={() => setActiveNavbar('#qualification')} className={activeNavbar === '#qualification' ? "nav__link active-link" : "nav__link"}>
+                <a href="#qualification" onClick={() => {setActiveNavbar('#qualification'); showMenu(!Toggle);}} className={activeNavbar === '#qualification' ? "nav__link active-link" : "nav__link"}>
                   <i className="uil uil-briefcase-alt nav__icon"></i>Qualification
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#portfolio" onClick={() => setActiveNavbar('#portfolio')} className={activeNavbar === '#portfolio' ? "nav__link active-link" : "nav__link"}>
+                <a href="#portfolio" onClick={() => {setActiveNavbar('#portfolio'); showMenu(!Toggle);}} className={activeNavbar === '#portfolio' ? "nav__link active-link" : "nav__link"}>
                   <i className="uil uil-scenery nav__icon"></i>Portfolio
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#contact" onClick={() => setActiveNavbar('#contact')} className={activeNavbar === '#contact' ? "nav__link active-link" : "nav__link"}>
+                <a href="#contact" onClick={() => {setActiveNavbar('#contact'); showMenu(!Toggle);}} className={activeNavbar === '#contact' ? "nav__link active-link" : "nav__link"}>
                   <i className="uil uil-message nav__icon"></i>Contact
                 </a>
               </li>
