019f9d352acf4998781103a96b1b24dfda33b554
SHOZAB ANWAR SIDDIQUE <s.nadeem014@yahoo.com>
2023-07-02 14:57:01 +0100

    Update className details.

--- portfolio/src/components/components/contact/Contact.jsx
+++ portfolio/src/components/components/contact/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
   return (
     <section className="contact section" id="contact">
         <h2 className="section__title">Contact Me</h2>
-        <p className="section__subtitle ">Contact me if you have a project proposal in mind, would like to work on a project with me or just want to say hello!</p>
+        <p className="section__subtitle container grid">Contact me if you have a project proposal in mind, would like to work on a project with me or just want to say hello!</p>
 
         <div className="contact__container container grid">
             <div className="contact__content">
